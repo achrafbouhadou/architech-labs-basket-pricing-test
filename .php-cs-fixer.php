cf2c92e68b481b12833db2918d320f7f9c6ba599
achrafbouhadou <achraf1323@gmail.com>
2025-10-23 05:59:45 +0100

    Set up project tooling

---
+++ .php-cs-fixer.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests')
+    ->in(__DIR__ . '/bin');
+
+return (new Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        'strict_param' => true,
+        'declare_strict_types' => true,
+    ])
+    ->setFinder($finder);
